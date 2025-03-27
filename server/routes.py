021225b37eeb4362d03db05edf534c284311055b
Suraj James <59333642+suraj-james@users.noreply.github.com>
2025-03-27 11:03:51 +0530

    Suraj

--- server/routes.py
+++ server/routes.py
@@ -4,6 +4,7 @@ from flask import request, render_template, make_response
 from server.webapp import flaskapp, cursor
 from server.models import Book
 
+//suraj is editing, and editing for good
 
 @flaskapp.route('/')
 def index():
